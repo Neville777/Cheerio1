6a3df626f3faa40ee498e8dd243d88ee304928bb
neville777 <neville.james@student.moringaschool.com>
2024-01-30 17:38:17 +0300

    getting details of product

--- crawler.js
+++ crawler.js
@@ -1,7 +1,6 @@
 import { gotScraping } from "got-scraping";
 import * as cheerio from "cheerio";
 
-// Split the base URL from the category to use it later.
 const WEBSITE_URL = "https://warehouse-theme-metal.myshopify.com";
 const storeUrl = `${WEBSITE_URL}/collections/sales`;
 
@@ -12,9 +11,31 @@ const $ = cheerio.load(html);
 
 const productLinks = $("a.product-item__title");
 
+// Prepare an empty array for our product URLs.
+const productUrls = [];
+
 for (const link of productLinks) {
   const relativeUrl = $(link).attr("href");
-  // Resolve relative URLs using the website's URL
   const absoluteUrl = new URL(relativeUrl, WEBSITE_URL);
-  console.log(absoluteUrl.href);
+
+  // Collect absolute product URLs.
+  productUrls.push(absoluteUrl);
+}
+
+// Loop over the stored URLs to process
+// each product page individually.
+for (const url of productUrls) {
+  // Download HTML.
+  const productResponse = await gotScraping(url);
+  const productHtml = productResponse.body;
+
+  // Load into Cheerio to parse the HTML.
+  const $productPage = cheerio.load(productHtml);
+
+  // Extract the product's title from the <h1> tag.
+  const productPageTitle = $productPage("h1").text().trim();
+
+  // Print the title to the terminal to see
+  // confirm we downloaded the correct pages.
+  console.log(productPageTitle);
 }
